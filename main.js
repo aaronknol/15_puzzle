4ee3be6e22319a8d30f848f961a374df1a8d4520
Aaron Knol <aaron@aaronknol@gmail.com>
2019-08-20 12:54:32 +1000

    Fixed cell styling to be absolutely positioned, fixed createElement style code to add 'px' to left and right, added blandCellCordinates variable to track the blank cell position

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ var game = () => {
 
     const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null];
 
+    let blankCellCordinates = [];
+
     // shuffle the 16 item array aroudn into random order
     // taken from stack overflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     const shuffle = (a) => {
@@ -25,8 +27,8 @@ var game = () => {
     const createElement = (x, y) => {
         const domElement = document.createElement('div');
         domElement.setAttribute('class', 'cell');
-        domElement.style.left= x * 50;
-        domElement.style.top = y * 50;
+        domElement.style.left= x * 50 + 'px';
+        domElement.style.top = y * 50 + 'px';
         domElement.x = x;
         domElement.y = y;
         domElement.textContent = game_board[y][x];
@@ -45,7 +47,14 @@ var game = () => {
                 //place the first element of the cells array on the board
                 
                 game_board[y][x] = shuffledCells[0];
-                board.appendChild(createElement(x, y));
+
+                // if the element at x,y is null (representing the 'blank' cell), don't create an element for it
+                if (game_board[y][x] !== null) {
+                    board.appendChild(createElement(x, y));
+                } else {
+                    blankCellCordinates = [y, x];
+                }
+
                 shuffledCells.shift();   // remove the first element which was just inserted onto the board
             }
         }
