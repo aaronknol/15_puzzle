559e64adc780e27d36d381fe3b4c82a218d01fb0
Aaron Knol <aaron@aaronknol@gmail.com>
2019-08-20 12:27:53 +1000

    Shuffled cells so they are randomly positioned on the board

--- main.js
+++ main.js
@@ -12,14 +12,27 @@ var game = () => {
 
     const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null];
 
+    // shuffle the 16 item array aroudn into random order
+    // taken from stack overflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    const shuffle = (a) => {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
     const generateBoard = () => {
         // loop through the 2 dimensional array from left to right, then top to bottom
         // inserting a number from cells array in the position currently looped to
+
+        const shuffledCells = shuffle(cells);
+
         for (var y=0; y <= 3; y++) {
             for (var x=0; x <= 3; x++) {
                 //place the first element of the cells array on the board
                 
-                game_board[y][x] = cells[0];
+                game_board[y][x] = shuffledCells[0];
                 var domElement = document.createElement('div');
                 domElement.setAttribute('class', 'cell');
                 domElement.style.left= x * 50;
@@ -28,7 +41,7 @@ var game = () => {
                 domElement.y = y;
                 domElement.textContent = game_board[y][x];
                 board.appendChild(domElement);
-                cells.shift();   // remove the first element which was just inserted onto the board
+                shuffledCells.shift();   // remove the first element which was just inserted onto the board
             }
         }
     }
